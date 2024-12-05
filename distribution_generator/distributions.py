17a44f41caa88ddd32efb816f01719ee85861852
AlbertoForesti <alberto.foresti.01@icloud.com>
2024-12-05 10:39:35 +0100

    Multiple dimensions possible

--- distribution_generator/distributions.py
+++ distribution_generator/distributions.py
@@ -2,6 +2,8 @@ from distribution_generator.evolution_lib import *
 from scipy.stats import rv_discrete
 from scipy.stats._multivariate import multi_rv_frozen
 
+import itertools
+
 class DistributionManager:
     def __init__(self):
         self.distribution_config = None
@@ -12,6 +14,8 @@ class DistributionManager:
                  mutual_information: float,
                  dim_x: int,
                  dim_y: int,
+                 seq_length_x: int = 1,
+                 seq_length_y: int = 1,
                  scale: float = 1.0,
                  loc: float = 0.0,
                  mean: np.array = None,
@@ -23,28 +27,72 @@ class DistributionManager:
                  min_val=0,
                  force_retrain=False) -> None:
 
-        if not force_retrain and self.distribution_config is not None and self.distribution_config == DistributionConfig(mutual_information, dim_x, dim_y, min_val):
-            return self.distribution
-        self.distribution_config = DistributionConfig(mutual_information, dim_x, dim_y, min_val)
-        self.evolution_task = EvolutionTask(mutual_information, dim_x, dim_y, scale, loc, mean, cov, strategy, mu, population_size, min_val)
-        self.evolution_task.train(n_generations)
-        self.distribution = self.evolution_task.best_agent.distribution
-        return self.distribution
+        if not force_retrain and self.distribution_config is not None and self.distribution_config == DistributionConfig(mutual_information, dim_x, dim_y, seq_length_x, seq_length_y, min_val):
+            return self.rv
+        self.distribution_config = DistributionConfig(mutual_information, dim_x, dim_y, seq_length_x, seq_length_y, min_val)
+        self.train_tasks(scale, loc, mean, cov, strategy, mu, population_size, min_val, n_generations)
+        self.rv = JointDiscrete(self.distribution, vocabulary_x=self.possible_x_sequences, vocabulary_y=self.possible_y_sequences)
+        return self.rv
+
+    def train_tasks(self, scale, loc, mean, cov, strategy, mu, population_size, min_val, n_generations):
+        """
+        Function to train many tasks to reach the desired mutual information by stacking random variables
+        It only generates distributions with pairwise dependencies between tokens
+        """
+
+        seq_length_x = self.distribution_config.seq_length_x
+        seq_length_y = self.distribution_config.seq_length_y
+        mutual_information = self.distribution_config.mutual_information
+        dim_x = self.distribution_config.dim_x
+        dim_y = self.distribution_config.dim_y
+
+        assert self.distribution_config.seq_length_x == self.distribution_config.seq_length_y, "Currently only supports same sequence length for x and y"
+
+        relevant_dims = min(seq_length_x, seq_length_y)
+        unit_mutinfo = mutual_information / relevant_dims
+        self.evolution_tasks = []
+        self.distributions = []
+        for dim in range(relevant_dims):
+            task = EvolutionTask(unit_mutinfo, dim_x, dim_y, scale, loc, mean, cov, strategy, mu, population_size, min_val)
+            task.train(n_generations)
+            self.distributions.append(task.best_agent.distribution)
+        
+        dist = np.zeros((dim_x**seq_length_x, dim_y**seq_length_y))
+
+        self.possible_x_sequences = np.array(list(itertools.product(range(dim_x), repeat=relevant_dims)))
+        self.possible_y_sequences = np.array(list(itertools.product(range(dim_y), repeat=relevant_dims)))
+        
+        for idx_x, x in enumerate(self.possible_x_sequences):
+            for idx_y, y in enumerate(self.possible_y_sequences):
+                joint_prob = 1
+                for i in range(relevant_dims):
+                    joint_prob *= self.distributions[i][x[i], y[i]]
+                dist[idx_x, idx_y] = joint_prob
+        self.distribution = dist
 
 class DistributionConfig:
-    def __init__(self, mutual_information: float, dim_x: int, dim_y: int, min_val: float):
+    def __init__(self, mutual_information: float, dim_x: int, dim_y: int, seq_length_x: int, seq_length_y: int, min_val: float):
         self.mutual_information = mutual_information
         self.dim_x = dim_x
         self.dim_y = dim_y
         self.min_val = min_val
+        self.seq_length_x = seq_length_x
+        self.seq_length_y = seq_length_y
     
     def __eq__(self, value: object) -> bool:
-        return self.mutual_information == value.mutual_information and self.dim_x == value.dim_x and self.dim_y == value.dim_y and self.min_val == value.min_val
+        return self.mutual_information == value.mutual_information and\
+               self.dim_x == value.dim_x and\
+               self.dim_y == value.dim_y and\
+               self.min_val == value.min_val and\
+               self.seq_length_x == value.seq_length_x and\
+               self.seq_length_y == value.seq_length_y
 
 class JointDiscrete(multi_rv_frozen):
 
-    def __init__(self, joint_dist, *args, **kwargs):
+    def __init__(self, joint_dist, *args, vocabulary_x=None, vocabulary_y=None, **kwargs):
         self.joint_dist = joint_dist
+        self.vocab_x = vocabulary_x
+        self.vocab_y = vocabulary_y
         pmf = joint_dist.flatten()
         values = np.arange(len(pmf))
         self._hidden_univariate = rv_discrete(name="hidden_univariate", values=(values, pmf))
@@ -59,19 +107,34 @@ class JointDiscrete(multi_rv_frozen):
         samples = self._hidden_univariate.rvs(*args, **kwargs)
         samples = np.unravel_index(samples, self.joint_dist.shape)
         samples = np.stack(samples, axis=1)
+        
         X = samples[:,0].reshape(-1,1)
+        if self.vocab_x is not None:
+            X = self.vocab_x[X]
+        
         Y = samples[:,1].reshape(-1,1)
+        if self.vocab_y is not None:
+            Y = self.vocab_y[Y]
+        
         return X, Y
     
     @property
-    def pmf(self):
-        return self._hidden_univariate.pmf(np.arange(len(self.joint_dist.flatten()))).reshape(2,-1, 1)
+    def entropy(self):
+        return -np.sum(self.joint_dist * np.log(self.joint_dist))
+    
+    @property
+    def mutual_information(self):
+        marginal_x = np.sum(self.joint_dist, axis=1)
+        marginal_y = np.sum(self.joint_dist, axis=0)
+        return np.sum(self.joint_dist * np.log(self.joint_dist / np.outer(marginal_x, marginal_y)))
 
 distribution_manager = DistributionManager()
 
 def get_rv(mutual_information: float,
                      dim_x: int,
                      dim_y: int,
+                     seq_length_x: int = 1,
+                     seq_length_y: int = 1,
                      scale: float = 1.0,
                      loc: float = 0.0,
                      mean: np.array = None,
@@ -82,11 +145,13 @@ def get_rv(mutual_information: float,
                      n_generations=100,
                      min_val=0,
                      force_retrain=False) -> None:
-    assert mutual_information <= min(np.log(dim_x), np.log(dim_y)), f"Mutual information is too high for the given dimensions, max is {min(np.log(dim_x), np.log(dim_y))} nats"
+    assert mutual_information <= min(np.log(dim_x**seq_length_x), np.log(dim_y**seq_length_y)), f"Mutual information is too high for the given dimensions, max is {min(np.log(dim_x**seq_length_x), np.log(dim_y**seq_length_y))} nats"
     assert mutual_information >= 0, "Mutual information must be non-negative"
-    dist = distribution_manager(mutual_information,
+    custom_rv = distribution_manager(mutual_information,
                                 dim_x,
                                 dim_y,
+                                seq_length_x,
+                                seq_length_y,
                                 scale,
                                 loc,
                                 mean,
@@ -97,5 +162,4 @@ def get_rv(mutual_information: float,
                                 n_generations,
                                 min_val,
                                 force_retrain)
-    custom_rv = JointDiscrete(dist)
     return custom_rv
